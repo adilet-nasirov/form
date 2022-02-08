42ba0137163f5f0a3a15daa4da47acdf84612acf
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-02-08 09:16:19 -0800

    created tip calculator, added funcs, styled els

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      serviceLevel: 30,
+      bill: "",
+      numOfPeople: "",
+      tip: "",
+      clicked: false,
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  calculateTips = (e) => {
+    const { serviceLevel, bill, numOfPeople } = this.state;
+    let tipAmount = (bill * serviceLevel) / 100 / numOfPeople;
+    this.setState({ tip: tipAmount, clicked: !this.state.clicked });
+  };
+
+  changeState = (e) => {
+    let name = e.target.name;
+    this.setState({ [name]: +e.target.value });
+  };
+  render() {
+    console.log(this.state);
+    return (
+      <div className="App">
+        <div className="container">
+          <h1>Tip Calculator</h1>
+          <p>How much was your bill?</p>
+          <label htmlFor="#">$</label>
+          <input
+            type="number"
+            name="bill"
+            placeholder="Bill amount"
+            onChange={this.changeState}
+          />
+          <p>How was your service?</p>
+          <select name="serviceLevel" id="serve" onChange={this.changeState}>
+            <option value="30">Great 30%</option>
+            <option value="20">Good 20%</option>
+            <option value="10">Normal 10%</option>
+          </select>
+          <p>How many people are sharing the bill?</p>
+          <input
+            onChange={this.changeState}
+            type="number"
+            placeholder="number of people"
+            name="numOfPeople"
+          />
+          <label htmlFor="">People</label>
+          <button className="btn" onClick={this.calculateTips}>
+            Calculate
+          </button>
+
+          <p
+            style={
+              this.state.clicked ? { display: "block" } : { display: "none" }
+            }
+          >
+            Tip for each: $<span>{this.state.tip}</span>
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
